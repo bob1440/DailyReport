bdeff99c9f24f4a2e9c51f5d491eb2b9830edfe1
PUWU <bob1440@gmail.com>
2014-12-16 00:39:09 +0800

    測試Post

--- DailyReport/Controllers/DailyController.cs
+++ DailyReport/Controllers/DailyController.cs
@@ -12,7 +12,21 @@ namespace DailyReport.Controllers
         // GET: /Daily/
         public ActionResult Index()
         {
+            Response.Write("Get:");
             return View();
         }
-	}
+
+        [HttpPost]
+        public ActionResult Index(string mission_tittle)
+        {
+            Response.Write("Post:");
+            Response.Write("P1:" + mission_tittle + "<br>");
+            Response.Write("P[0]:" + Request.Form[0] + "<br>");
+            Response.Write("P[1]:" + Request.Form[1] + "<br>");
+
+            Response.Write("sum:" + Request.Form.Count);
+
+            return View();
+        }
+    }
 }
