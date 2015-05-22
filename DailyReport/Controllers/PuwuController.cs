3c75ee8d7abca596922b50b3dcf127c3510dd627
PUWU <bob1440@gmail.com>
2015-05-22 21:51:31 +0900

    加blog內頁

--- DailyReport/Controllers/PuwuController.cs
+++ DailyReport/Controllers/PuwuController.cs
@@ -20,9 +20,12 @@ namespace DailyReport.Controllers
 
         public ActionResult Blog()
         {
-
             return View();
+        }
 
+        public ActionResult Article()
+        {
+            return View();
         }
 
 
